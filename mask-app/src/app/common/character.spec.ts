e6bfc4388f0d18d1a8667af80318547e40ca495d
Ravi J <raviregi@gmail.com>
2022-03-21 18:53:34 +0530

    refactored character.spec.ts

--- mask-app/src/app/common/character.spec.ts
+++ mask-app/src/app/common/character.spec.ts
@@ -2,6 +2,6 @@ import { Character } from './character';
 
 describe('Character', () => {
   it('should create an instance', () => {
-    expect(new Character()).toBeTruthy();
+    expect(new Character("Harry","James","Potter")).toBeTruthy();
   });
 });
